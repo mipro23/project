b16779273ce7431fc9999e2c2fb90a0789a2473d
mipro23 <s27i_osipenko@179.ru>
2023-06-12 10:38:24 +0300

    message

--- main2.py
+++ main2.py
@@ -9,21 +9,45 @@ from aiogram.dispatcher import FSMContext
 # Initialize bot and dispatcher
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
-
+searching = ''
 connected_users = []
-talking_users = []
+talking_users = dict(
+    
+)
+
+
 @dp.message_handler(commands=['start', 'help'], state='*')
 async def send_welcome(message: types.Message, state: FSMContext):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
+    """This handler will be called when user sends /start or /help command"""
     await message.answer("Hi!\nI'm EchoBot!\nPowered by aiogram.\nPlease, say your name")
     await state.set_state("q1")
+
 
 @dp.message_handler(commands=['online'], state='*')
 async def send_welcome(message: types.Message, state: FSMContext):
     await message.answer(f"There is {len(connected_users)} online.")
 
+
+@dp.message_handler(commands=['find'], state='*')
+async def find(message: Message, state: FSMContext):
+    global searching, talking_users
+    if searching == '' or searching == message.from_user.id:
+        searching = message.from_user.id
+        await message.answer('Waiting...')
+    else:
+        await message.answer('Connected!')
+        await bot.send_message(searching, 'Connected!')
+        talking_users[message.from_user.id] = searching
+        talking_users[searching] = message.from_user.id
+        await state.set_state('talking')
+        target_state = dp.current_state(chat=searching, user=searching)
+        await target_state.set_state('talking')
+        searching = ''
+
+@dp.message_handler(state='talking')
+async def find(message: Message, state: FSMContext):
+    sending = talking_users[message.from_user.id]
+    await bot.send_message(sending, message.text)
 
 
 @dp.message_handler(state="q1")
@@ -48,15 +72,24 @@ async def process_age(message: types.Message, state: FSMContext):
         await message.answer(f"This is not a number, try another time {data['name']}")
 
 
-@dp.message_handler(commands=['find'], state='*')
-async def find(message: Message):
-    
-@dp.message_handler(state='*')
-async def echo(message: Message):
-    for user in connected_users:
-        if message.from_user.id == user:
-            continue
-        await bot.send_message(user, message.text)
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
